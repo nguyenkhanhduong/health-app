e897b62096fe2ae45a366b6e9b2e923a900d60dd
nguyenkhanhduong <50391104+nguyenkhanhduong@users.noreply.github.com>
2025-08-20 13:58:23 +0700

    fix: update MainLayout component structure for improved layout consistency

--- react-app/src/components/layouts/main/index.tsx
+++ react-app/src/components/layouts/main/index.tsx
@@ -6,10 +6,11 @@ import { Header } from '@/components/layouts/main/header'
 import { ScrollTopButton } from '@/components/layouts/main/sroll-top-button'
 
 export const MainLayout: FC = () => (
-  <div className='bg-white'>
+  <div className='flex min-h-screen flex-col bg-white'>
     <Header />
     <main
       role='main'
+      className='flex-1'
     >
       <Outlet />
     </main>
